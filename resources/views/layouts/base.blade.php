6514be749abb993d35e0ed1b64a6d53d29e2675c
Iammutevu <ianmutevu96@gmail.com>
2020-06-23 04:07:56 +0300

    Top 3 Fetched

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -7,11 +7,17 @@
     <link rel="stylesheet" href="{{ asset('css/style.css')}}">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="http://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="http://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
 </head>
 <body>
+	<!-- As a link -->
+<nav class="navbar navbar-light bg-light">
+  <a class="ml-auto btn btn-outline-secondary" href="/login">Login</a>
+</nav>
+
     @yield('content')
 </body>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,38 +1,32 @@
 @extends('layouts.base')
 @section('content')
 <div class="jumbotron">
-  <h1 class="display-4">HNG Leaderboard</h1>
+  <h1 class="display-4"><img src="{{ asset('images/hng-logo.jpg')}}"> HNG Leaderboard</h1>
   <hr class="my-4">
   <div class="container">
       <div class="row">
           <div class="col-md-4">
             <div class="card">
               <img src="{{ asset('images/user.jpg')}}" class=" fit card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">Name: {{ $top[0]->name }}</h5>
-                <h5 class="card-title">Slack: {{ $top[0]->slack_username }}</h5>
-                <h5 class="card-title">Points: {{ $top[0]->points }}</h5>
-              </div>
+              <div class="card-footer"><i class="fa fa-slack fa-lg" aria-hidden="true"></i>
+{{ $top[0]->slack_username }} <span class="badge badge-info">{{ $top[0]->points }}</span>
+                </div>
             </div>         
           </div>
           <div class="col-md-4">
             <div class="card">
               <img src="{{ asset('images/user2.jpg')}}" class="fit card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">Name: {{ $top[1]->name }}</h5>
-                <h5 class="card-title">Slack: {{ $top[1]->slack_username }}</h5>
-                <h5 class="card-title">Points: {{ $top[1]->points }}</h5>
-              </div>
+                <div class="card-footer"><i class="fa fa-slack fa-lg" aria-hidden="true"></i>
+{{ $top[1]->slack_username }} <span class="badge badge-info">{{ $top[1]->points }}</span>
+                </div>
             </div>         
           </div>
           <div class="col-md-4">
             <div class="card">
               <img src="{{ asset('images/user3.jpg')}}" class="fit card-img-top" alt="...">
-              <div class="card-body">
-                <h5 class="card-title">Name: {{ $top[2]->name }}</h5>
-                <h5 class="card-title">Slack: {{ $top[2]->slack_username }}</h5>
-                <h5 class="card-title">Points: {{ $top[2]->points }}</h5>
-              </div>
+              <div class="card-footer"><i class="fa fa-slack fa-lg" aria-hidden="true"></i>
+{{ $top[2]->slack_username }} <span class="badge badge-info">{{ $top[2]->points }}</span>
+                </div>
             </div>         
           </div>
 
@@ -67,7 +61,7 @@
             </tr>
         </tfoot>
         <tbody>
-             
+            @if(count($interns) > 0)
             @foreach($interns as $intern)
                 <tr>
                     <td></td>
@@ -76,17 +70,22 @@
                     <td>{{ $intern->email }}</td>
                     <td>{{ $intern->points }}</td>
                     <td>
-                        <button class="btn btn-info">Share</button>
+                        <a href="#" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u='+window.location.href, 'Facebook Share', 'width=620, height=420'); return false;" class="fa fa-social fa-facebook fa-align-center special_link"></a>
+                        <a href="#" data-size="large" class="fa fa-social fa-twitter special_link" id="twitter_sharer"></a>
                     </td>
                     <input type="text" class="intern_id" id="intern_id" value="{{ $intern->id}}" hidden>
                 </tr>
             @endforeach
+            @endif
         </tbody>
     </table>
 </div>
 <script>
     $(document).ready(function(){       
         $('#myTable').DataTable();
+
+        //Twitter sharer button
+      $("#twitter_sharer").attr('href', "https://twitter.com/intent/tweet?text=Checkout my rank in the HNG Leaderboard here:"+window.location.href);
     });
 </script>
 
